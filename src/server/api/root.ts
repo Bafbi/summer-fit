da75b4e9f9d2fe41701a7d177a6dc911373c9545
Axel BOCHU <axel.bochu@student.junia.com>
2024-01-15 15:59:08 +0100

    creation API Salle de sport

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
+import {hallsRouter} from "~/server/api/routers/halls";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  halls: hallsRouter,
 });
 
 // export type definition of API
