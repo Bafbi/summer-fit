d76563f28746617591009f80842dd7553ea244ac
Paul POUSSET <paul.pousset@student.junia.com>
2024-01-17 16:43:47 +0100

    popup qrcode etc

--- src/app/_components/organisms/Map.tsx
+++ src/app/_components/organisms/Map.tsx
@@ -366,7 +366,7 @@ const handleSalleClick = (salle: Salle) => {
             </p>
           <button
             onClick={handleReservationClick}
-            className="mt-4 bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded"
+            className="mt-4 py-2 px-4"
           >
             Réserver un créneau
           </button>
