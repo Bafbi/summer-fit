6631faedab2c83fe39a817becac131898dd72359
Paul POUSSET <paul.pousset@student.junia.com>
2024-01-18 14:44:40 +0100

    style abo

--- src/app/abonnement/page.tsx
+++ src/app/abonnement/page.tsx
@@ -1,12 +1,16 @@
 import Footer from "../_components/organisms/Footer";
 import Header from "../_components/organisms/Header";
 import React from "react";
-import "~/styles/accueil.css"; // Import your CSS file
+import "~/styles/accueil.css"; 
+import "~/styles/header.css"; 
+// Import your CSS file
 import Or from "/public/assets/images/or.svg";
 import Argent from "/public/assets/images/argent.svg";
 import Diamand from "/public/assets/images/diamond.svg";
 import Image from "next/image";
 import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+import { AbonneBouton } from "../_components/api/abonne_bouton";
 
 const App = async () => {
   const session = await getServerAuthSession();
@@ -16,6 +20,9 @@ const App = async () => {
     <>
       <Header></Header>
 
+<main className="">
+  
+
       <section className="py-16">
         <div className="container mx-auto px-4">
           <h1 className="mb-6 pt-4 text-3xl font-semibold">
@@ -24,7 +31,7 @@ const App = async () => {
 
           <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
             {/* Carte d'offre 1 */}
-            <div className="rounded-lg bg-white p-6 shadow-md">
+            <div className="rounded-lg bg-[#eeeff0] p-6 shadow-md">
               <div className="text-center">
                 <Image
                   src={Argent}
@@ -32,22 +39,22 @@ const App = async () => {
                   className="mx-auto mb-4 h-32 w-32 rounded-full"
                 />
               </div>
-              <h3 className="mb-2 text-center text-xl font-semibold text-primary">
+              <h3 className="mb-2 text-center text-xl font-semibold text-[#7945f7]">
                 <span>ARGENT</span>
               </h3>
-              <ul className="text-gray-600 mb-4 text-left pl-6">
+              <ul className="mb-4 pl-6 text-left text-gray-600">
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">
                     Accès toutes nos salles de sport
                   </span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-red-500 mr-2">&#10006;</span>
+                  <span className="mr-2 text-red-500">&#10006;</span>
                   <span className="text-red-500">Coach virtuel illimité</span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-red-500 mr-2">&#10006;</span>
+                  <span className="mr-2 text-red-500">&#10006;</span>
                   <span className="text-red-500">
                     Cours avec nos coachs Summer-Fit
                   </span>
@@ -55,21 +62,30 @@ const App = async () => {
               </ul>
               {userAbonnement === "ARGENT" ? (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <button className="bg-[#ad57f9] text-white px-8 py-2 rounded-lg transition-transform transform hover:bg-[#7945f7] active:scale-95"
+                  >
                     Abonnement actuel
                   </button>
+                  <br />
+                  <span className="text[#ad57f9]-500 mt-1">
+                   20€/mois
+                  </span>
                 </div>
               ) : (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <AbonneBouton plan="ARGENT">
                     Souscrire
-                  </button>
+                  </AbonneBouton>
+                  <br />
+                  <span className="text-[#ad57f9]-500 mt-1">
+                   20€/mois
+                  </span>
                 </div>
               )}
             </div>
 
             {/* Carte d'offre 2 */}
-            <div className="rounded-lg bg-white p-6 shadow-md">
+            <div className="rounded-lg bg-[#eeeff0] p-6 shadow-md">
               <div className="text-center">
                 <Image
                   src={Or}
@@ -77,22 +93,22 @@ const App = async () => {
                   className="mx-auto mb-4 h-32 w-32 rounded-full"
                 />
               </div>
-              <h3 className="mb-2 text-center text-xl font-semibold text-primary">
+              <h3 className="mb-2 text-center text-xl font-semibold text-[#7945f7]">
                 <span>Or</span>
               </h3>
-              <ul className="text-gray-600 mb-4 text-left pl-6">
+              <ul className="mb-4 pl-6 text-left text-gray-600">
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">
                     Accès toutes nos salles de sport
                   </span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">Coach virtuel illimité</span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-red-500 mr-2">&#10006;</span>
+                  <span className="mr-2 text-red-500">&#10006;</span>
                   <span className="text-red-500">
                     Cours avec nos coachs Summer-Fit
                   </span>
@@ -100,21 +116,29 @@ const App = async () => {
               </ul>
               {userAbonnement === "OR" ? (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <button className="bg-[#ad57f9] text-white px-8 py-2 rounded-lg transition-transform transform hover:bg-[#7945f7] active:scale-95">
                     Abonnement actuel
                   </button>
+                  <br />
+                  <span className="text-[#ad57f9]-500 mt-1">
+                   35€/mois
+                  </span>
                 </div>
               ) : (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <AbonneBouton plan="OR">
                     Souscrire
-                  </button>
+                  </AbonneBouton>
+                  <br />
+                  <span className="text-blue-500 mt-1">
+                   35€/mois
+                  </span>
                 </div>
               )}
             </div>
 
             {/* Carte d'offre 3 */}
-            <div className="rounded-lg bg-white p-6 shadow-md">
+            <div className="rounded-lg bg-[#eeeff0] p-6 shadow-md">
               <div className="text-center">
                 <Image
                   src={Diamand}
@@ -122,22 +146,22 @@ const App = async () => {
                   className="mx-auto mb-4 h-32 w-32 rounded-full"
                 />
               </div>
-              <h3 className="mb-2 text-center text-xl font-semibold text-primary">
+              <h3 className="mb-2 text-center text-xl font-semibold text-[#7945f7]">
                 <span>Diamant</span>
               </h3>
-              <ul className="text-gray-600 mb-4 text-left pl-6">
+              <ul className="mb-4 pl-6 text-left text-gray-600">
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">
                     Accès toutes nos salles de sport
                   </span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">Coach virtuel illimité</span>
                 </li>
                 <li className="mb-2">
-                  <span className="text-green-500 mr-2">&#10004;</span>
+                  <span className="mr-2 text-green-500">&#10004;</span>
                   <span className="text-green-500">
                     Cours avec nos coachs Summer-Fit
                   </span>
@@ -145,21 +169,30 @@ const App = async () => {
               </ul>
               {userAbonnement === "DIAMANT" ? (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <button className="bg-[#ad57f9] text-white px-8 py-2 rounded-lg transition-transform transform hover:bg-[#7945f7] active:scale-95">
                     Abonnement actuel
                   </button>
+                  <br />
+                  <span className="text-blue-500 mt-1">
+                   50€/mois
+                  </span>
                 </div>
               ) : (
                 <div className="text-center">
-                  <button className="bg-primary rounded-lg px-4 py-2 text-white hover:bg-primary-dark">
+                  <AbonneBouton plan="DIAMANT">
                     Souscrire
-                  </button>
+                  </AbonneBouton>
+                  <br />
+                  <span className="text-blue-500 mt-1">
+                   50€/mois
+                  </span>
                 </div>
               )}
             </div>
           </div>
         </div>
       </section>
+      </main>
 
       <Footer></Footer>
     </>
