7579494de531103a20872c1048d3dde195fb9384
Bafbi <bafbi3@gmail.com>
2024-01-19 10:49:09 +0100

    maybe ?

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -69,7 +69,7 @@ export const authOptions: NextAuthOptions = {
       if (userExists) {
         return true;   //if the email exists in the User collection, email them a magic login link
       } else {
-        return "/register";
+        return "/client/info";
       }
 
      
